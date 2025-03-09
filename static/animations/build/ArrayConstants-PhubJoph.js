896252c5a756037f15bde3579080ee66309db35a
dsmtE <desmet.enguerrand@gmail.com>
2025-03-09 19:42:29 +0100

    ✨ Update animations

---
+++ static/animations/build/ArrayConstants-PhubJoph.js
@@ -0,0 +1,1 @@
+const t={boxGap:28,outlineMargin:16,boxWidth:128,boxRadius:4,boxStrokeWidth:8},b=o=>o.boxWidth+o.boxGap;export{t as S,b};
---
+++ static/animations/build/useRandom-BZdmpElt.js
@@ -0,0 +1,1 @@
+import{ab as o,u as n}from"./_virtual_settings-W9jktp4-.js";function t(e,a=!0){return typeof e=="number"?new o(a?e:e+n().meta.seed.get()):n().random}export{t as u};
